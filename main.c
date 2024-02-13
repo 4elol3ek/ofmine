2918cc3bf478705db7ce23705fb44ece2db3b699
Данияр Тилепов <is342s24@bwp07.ipa.csc.sibsutis.ru>
2024-02-13 13:13:31 +0700

    create main.c realizes reading from tesfile2

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main(){
-    printf("HEllo, world");
+typedef struct {
+    float xMiddle;
+    float yMiddle;
+    float rad;
+} fCircle;
+
+int main(int argc, char* argv[]){
+    char charbuffer[256];
+    FILE *file = fopen(argv[argc - 1], "r");
+    while ((fgets (charbuffer, 256, file)) != NULL){
+        printf ("%s", charbuffer);
+    };
     return 0;
 }

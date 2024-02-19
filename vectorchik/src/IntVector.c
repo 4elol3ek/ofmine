54b79c5870036b8a00ae6ba3a932ab37ff75b4ce
Данияр Тилепов <is342s24@bwp07.ipa.csc.sibsutis.ru>
2024-02-19 14:10:38 +0700

    create proglabfolder

---
+++ vectorchik/src/IntVector.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+typedef struct IntVector{
+    size_t size;
+    size_t capacity;
+    int* zero_pointer;
+} IntVector;
+
+IntVector* int_vector_new(size_t initial_capacity){
+    
+}
